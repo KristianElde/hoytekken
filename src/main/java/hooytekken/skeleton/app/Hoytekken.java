2d7679b66ddcd8a25586500316a80877f2ac6bf1
Mathias Ness <mathias1389@gmail.com>
2024-02-28 13:14:12 +0100

    Added Game Application listener for Hoytekken game

---
+++ src/main/java/hooytekken/skeleton/app/Hoytekken.java
@@ -0,0 +1,22 @@
+package hooytekken.skeleton.app;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import hooytekken.skeleton.app.view.GameScreen;
+
+public class Hoytekken extends Game {
+    public static final int TILE_SIZE = 32;
+    public static final int V_WIDTH = TILE_SIZE*30;
+    public static final int V_HEIGHT = TILE_SIZE*15;
+    public static final float PPM = 100;
+
+    public SpriteBatch batch;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        //setScreen(new GameScreen(this));
+        
+    }
+    
+}
