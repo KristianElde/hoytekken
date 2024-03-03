315e1b3ae6e43cd13d4651377c0180d46df5a82a
Mathias Ness <mathias1389@gmail.com>
2024-03-03 23:50:58 +0100

    added enum ForceDirection

---
+++ src/main/java/hooytekken/skeleton/app/model/components/ForceDirection.java
@@ -0,0 +1,10 @@
+package hooytekken.skeleton.app.model.components;
+
+/**
+ * Enum for the directions of a horisontal force
+ */
+public enum ForceDirection {
+    STATIC, 
+    LEFT, 
+    RIGHT
+}
