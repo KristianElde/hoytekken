9f4b09482bdf8e2c0082ef698d3cb4fe3fab04d7
Mathias Ness <mathias1389@gmail.com>
2024-03-03 23:27:49 +0100

    changed class to extend InputAdapter, this allows for removing unused @override methods

--- src/main/java/hooytekken/skeleton/app/controller/HtekkenController.java
+++ src/main/java/hooytekken/skeleton/app/controller/HtekkenController.java
@@ -1,13 +1,14 @@
 package hooytekken.skeleton.app.controller;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.InputProcessor;
 
 /**
  * Controller for the game
  * handles input from the user
  */
-public class HtekkenController implements InputProcessor {
+public class HtekkenController extends InputAdapter {
     //ControllableModel model;
 
     public HtekkenController(/*ControllableModel model*/) {
@@ -27,46 +28,5 @@ public class HtekkenController implements InputProcessor {
         return false;
     }
 
-    @Override
-    public boolean keyTyped(char character) {
-        //ignore implementation key typed
-        return false;
-    }
-
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        //ignore implementation mouse click
-        return false;
-    }
-
-    @Override
-    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
-        ///ignore implementation mouse click up
-        return false;
-    }
-
-    @Override
-    public boolean touchCancelled(int screenX, int screenY, int pointer, int button) {
-        //ignore implementation mouse click cancelled
-        return false;
-    }
-
-    @Override
-    public boolean touchDragged(int screenX, int screenY, int pointer) {
-        //ignore implementation mouse dragged
-        return false;
-    }
-
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        //ignore implementation mouse moved
-        return false;
-    }
-
-    @Override
-    public boolean scrolled(float amountX, float amountY) {
-        //ignore implementation mouse scrolled
-        return false;
-    }
-    
+     
 }
