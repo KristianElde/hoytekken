6c3e2fc246e869a93bb223cc648c87b345dd2ca2
Mathias Ness <mathias1389@gmail.com>
2024-03-03 23:47:31 +0100

    added methods setDirection and jump to Controllable model interface

--- src/main/java/hooytekken/skeleton/app/controller/ControllableModel.java
+++ src/main/java/hooytekken/skeleton/app/controller/ControllableModel.java
@@ -5,4 +5,18 @@ package hooytekken.skeleton.app.controller;
  */
 public interface ControllableModel {
     
+    /**
+     * Set the direction of the given player
+     * @param player the player to set the direction for
+     * @param direction the direction to apply force
+     * @return true if the direction was set, false otherwise
+     */
+    public boolean setDirection(int player, int direction);
+
+    /**
+     * Make the given player jump
+     * @param player the player to make jump
+     * @return true if the player jumped, false otherwise
+     */
+    public boolean jump(int player);
 }
