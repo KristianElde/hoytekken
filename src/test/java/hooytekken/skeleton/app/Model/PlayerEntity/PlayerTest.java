19466d8717cb56340f060c0dcc0a0f7bf1717353
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-05 13:35:07 +0100

    implemented sanitytest then started on testMove and made the move forward test

--- src/test/java/hooytekken/skeleton/app/Model/PlayerEntity/PlayerTest.java
+++ src/test/java/hooytekken/skeleton/app/Model/PlayerEntity/PlayerTest.java
@@ -1,7 +1,14 @@
 package hooytekken.skeleton.app.Model.PlayerEntity;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Random;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.badlogic.gdx.physics.box2d.World;
 
@@ -13,6 +20,7 @@ import hooytekken.skeleton.app.model.components.PlayerEntity.Player;
 public class PlayerTest {
     private World world;
     private Player player;
+    private Random rand = new Random();
 
     @BeforeEach
     void setUpBeforeEach() {
@@ -20,4 +28,29 @@ public class PlayerTest {
         world = new World(null, false); // which values here?
         player = new Player(world, null); // dont need playertype?
     }
+
+    @Test
+    void sanityTest() {
+        assertNotNull(player);
+        assertTrue(player.isAlive());
+        assertEquals(100, player.getHealth());
+    }
+
+    @Test
+    void testMove() {
+        float initX = player.getBody().getPosition().x;
+        float initY = player.getBody().getPosition().y;
+        float randX = rand.nextFloat(0.1f, 1.0f);
+        float randY = rand.nextFloat(0.1f, 1.0f);
+
+        // move forward
+        player.move(randX, randY);
+        assertTrue(player.getBody().getPosition().x > initX);
+        assertTrue(player.getBody().getPosition().y > initY);
+        assertEquals(initX + randX, player.getBody().getPosition().x);
+        assertEquals(initY + randY, player.getBody().getPosition().y);
+
+        // move back to origin
+    }
+
 }
