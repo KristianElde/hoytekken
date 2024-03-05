06f5353bb992199c346cf02018acb010e7cba853
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-05 13:11:08 +0100

    add world and player to setUpBeforeEach

--- src/test/java/hooytekken/skeleton/app/Model/PlayerEntity/PlayerTest.java
+++ src/test/java/hooytekken/skeleton/app/Model/PlayerEntity/PlayerTest.java
@@ -1,8 +1,23 @@
 package hooytekken.skeleton.app.Model.PlayerEntity;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+import hooytekken.skeleton.app.model.components.PlayerEntity.Player;
+
 /**
  * Unit tests for the Player class
  */
 public class PlayerTest {
+    private World world;
+    private Player player;
 
+    @BeforeEach
+    void setUpBeforeEach() {
+        // create interface for world which gives better abstraction?
+        world = new World(null, false); // which values here?
+        player = new Player(world, null); // dont need playertype?
+    }
 }
