3f53f5e484ed0d50d8cbbc6c342982be6853eba2
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-06 11:07:18 +0100

    made IPlayer extend ICombat

--- src/main/java/hooytekken/skeleton/app/model/components/PlayerEntity/IPlayer.java
+++ src/main/java/hooytekken/skeleton/app/model/components/PlayerEntity/IPlayer.java
@@ -5,7 +5,7 @@ import com.badlogic.gdx.physics.box2d.Body;
 /**
  * Interface defining common behavior for a player in the game.
  */
-public interface IPlayer {
+public interface IPlayer extends ICombat {
 
     /**
      * 
