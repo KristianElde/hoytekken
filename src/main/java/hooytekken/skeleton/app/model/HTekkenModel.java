e811e4affde3030b8a61749a61eacd715307bedc
Mathias Ness <mathias1389@gmail.com>
2024-03-06 14:12:00 +0100

    getworld and getplayer methods in ViewableModel interface. HtekkenModel implements ViewableModel.

--- src/main/java/hooytekken/skeleton/app/model/HTekkenModel.java
+++ src/main/java/hooytekken/skeleton/app/model/HTekkenModel.java
@@ -6,8 +6,9 @@ import com.badlogic.gdx.physics.box2d.World;
 import hooytekken.skeleton.app.model.components.PlayerEntity.IPlayer;
 import hooytekken.skeleton.app.model.components.PlayerEntity.Player;
 import hooytekken.skeleton.app.model.components.PlayerEntity.PlayerType;
+import hooytekken.skeleton.app.view.ViewableModel;
 
-public class HTekkenModel {
+public class HTekkenModel implements ViewableModel {
     private static final String DEFAULT_MAP = "defaultMap.tmx";
     private World gameWorld;
 
@@ -22,10 +23,10 @@ public class HTekkenModel {
      */
     public HTekkenModel(String map) {
         this.map = map;
-        gameWorld = new World(new Vector2(0, -20), true);
+        this.gameWorld = new World(new Vector2(0, -20), true);
 
-        player1 = new Player(gameWorld, PlayerType.PLAYER_ONE);
-        player2 = new Player(gameWorld, PlayerType.PLAYER_TWO);
+        this.player1 = new Player(gameWorld, PlayerType.PLAYER_ONE);
+        this.player2 = new Player(gameWorld, PlayerType.PLAYER_TWO);
     }
 
     /**
@@ -33,5 +34,23 @@ public class HTekkenModel {
      */
     public HTekkenModel() {
         this(DEFAULT_MAP);
+    }
+
+    @Override
+    public void updateModel(float dt) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateModel'");
+    }
+
+    @Override
+    public World getGameWorld() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGameWorld'");
+    }
+
+    @Override
+    public IPlayer getPlayer(int playerNumber) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPlayer'");
     }
 }
--- src/main/java/hooytekken/skeleton/app/view/ViewableModel.java
+++ src/main/java/hooytekken/skeleton/app/view/ViewableModel.java
@@ -1,5 +1,27 @@
 package hooytekken.skeleton.app.view;
 
+import com.badlogic.gdx.physics.box2d.World;
+
+import hooytekken.skeleton.app.model.components.PlayerEntity.IPlayer;
+
 public interface ViewableModel {
     
+    /**
+     * Updates the model
+     * @param dt time slice float
+     */
+    public void updateModel(float dt);
+
+    /**
+     * Getter for the game world
+     * @return the game world
+     */
+    public World getGameWorld();
+
+    /**
+     * Getter for the player
+     * @param playerNumber the player number
+     * @return the player
+     */
+    public IPlayer getPlayer(int playerNumber);
 }
