f936a59e5bc4c9f6b6f3459a0aed77b3dc6e312c
Lauritz Angeltveit <laang9684@uib.no>
2024-03-06 14:28:37 +0100

    La til en funksjon generateLayer som faktisk oppretter et layer gitt en index

--- src/main/java/hooytekken/skeleton/app/model/components/Box2DWorldGenerator.java
+++ src/main/java/hooytekken/skeleton/app/model/components/Box2DWorldGenerator.java
@@ -1,11 +1,16 @@
 package hooytekken.skeleton.app.model.components;
 
+import com.badlogic.gdx.maps.MapObject;
+import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
+
+import hooytekken.skeleton.app.Hoytekken;
 
 
 public class Box2DWorldGenerator {
@@ -26,10 +31,38 @@ public class Box2DWorldGenerator {
         PolygonShape shape = new PolygonShape();
         FixtureDef fdef = new FixtureDef();
         Body body = null;
+
+        //generate main platform
+        generateLayer(0, bdef, shape, fdef, body);
+
+        //generate other platforms
+        generateLayer(1, bdef, shape, fdef, body);
     }
 
+    /**
+     * Generate a layer of box2d bodies
+     * @param layerIndex Which layer to generate
+     * @param bdef 
+     * @param shape
+     * @param fdef
+     * @param body
+     */
+    private void generateLayer(int layerIndex, BodyDef bdef, PolygonShape shape, FixtureDef fdef, Body body) {
 
+        for (MapObject object : map.getLayers().get(layerIndex).getObjects().getByType(RectangleMapObject.class)) {
+            Rectangle rect = ((RectangleMapObject) object).getRectangle();
 
-    
+            bdef.type = BodyDef.BodyType.StaticBody;
+            bdef.position.set(
+                (rect.getX() + rect.getWidth()/2) / Hoytekken.PPM, 
+                (rect.getY() + rect.getHeight()/2) / Hoytekken.PPM);
+
+            body = world.createBody(bdef);
+
+            shape.setAsBox((rect.getWidth()/2) / Hoytekken.PPM, (rect.getHeight()/2) / Hoytekken.PPM);
+            fdef.shape = shape;
+            body.createFixture(fdef);
+        }
+    }
 }
 
