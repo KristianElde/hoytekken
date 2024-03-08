c0ee56d44cfe29dbc67661df5171f9087e2cbf73
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-03-08 11:15:10 +0100

    Made simple gameOverScreen

--- src/main/java/hooytekken/skeleton/app/view/GameOverScreen.java
+++ src/main/java/hooytekken/skeleton/app/view/GameOverScreen.java
@@ -1,15 +1,22 @@
 package hooytekken.skeleton.app.view;
 
+import javax.swing.text.TabSet;
+
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthoCachedTiledMapRenderer;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import hooytekken.skeleton.app.Hoytekken;
@@ -25,15 +32,7 @@ public class GameOverScreen implements Screen {
     private OrthographicCamera gameCam;
     private Viewport gamePort;
 
-    private TiledMap map;
-    private OrthoCachedTiledMapRenderer renderer;
-
-    private Texture welcomeImg;
-
-    private Box2DDebugRenderer b2dr;
-
-    private static final float WELCOME_TEXT_WIDTH = 300;
-    private static final float WELCOME_TEXT_HEIGHT = 300;
+    private Stage stage;
 
     /**
      * Constructor for the game screen
@@ -45,17 +44,17 @@ public class GameOverScreen implements Screen {
         this.model = model;
 
         gameCam = new OrthographicCamera();
-        gamePort = new FitViewport(Hoytekken.V_WIDTH / Hoytekken.PPM, Hoytekken.V_HEIGHT / Hoytekken.PPM, gameCam);
+        gamePort = new FitViewport(Hoytekken.V_WIDTH, Hoytekken.V_HEIGHT, gameCam);
 
-        map = model.getTiledMap();
-        renderer = new OrthoCachedTiledMapRenderer(map, 1 / Hoytekken.PPM);
+        stage = new Stage(gamePort, game.batch);
+        Label.LabelStyle font = new Label.LabelStyle(new BitmapFont(), Color.WHITE);
+        Table table = new Table();
+        table.center();
+        table.setFillParent(true);
+        Label gameOverLabel = new Label("Game Over", font);
 
-        welcomeImg = new Texture("game-over.jpg");
-
-        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
-
-        b2dr = new Box2DDebugRenderer();
-
+        table.add(gameOverLabel).expandX();
+        stage.addActor(table);
     }
 
     private void handleStateSwitch() {
@@ -65,9 +64,7 @@ public class GameOverScreen implements Screen {
     }
 
     private void update(float delta) {
-        model.updateModel(delta);
         gameCam.update();
-        renderer.setView(gameCam);
         handleStateSwitch();
     }
 
@@ -83,22 +80,12 @@ public class GameOverScreen implements Screen {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
-        renderer.render();
-
-        game.batch.setProjectionMatrix(gameCam.combined);
-
-        game.batch.begin();
-
-        game.batch.draw(welcomeImg, (Hoytekken.V_WIDTH / 2 - WELCOME_TEXT_WIDTH / 2) / Hoytekken.PPM,
-                (Hoytekken.V_HEIGHT / 2 - WELCOME_TEXT_HEIGHT / 2) / Hoytekken.PPM, WELCOME_TEXT_WIDTH / Hoytekken.PPM,
-                WELCOME_TEXT_HEIGHT / Hoytekken.PPM);
-
-        game.batch.end();
+        stage.draw();
     }
 
     @Override
     public void resize(int width, int height) {
-        gamePort.update(width, height);
+        // gamePort.update(width, height);
     }
 
     @Override
