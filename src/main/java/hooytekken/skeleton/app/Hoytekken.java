d0cd262acba88b2e360399885d17c5fb48212a09
Mathias Ness <mathias1389@gmail.com>
2024-03-08 12:31:28 +0100

    Documentation Hoytekken

--- src/main/java/hooytekken/skeleton/app/Hoytekken.java
+++ src/main/java/hooytekken/skeleton/app/Hoytekken.java
@@ -7,6 +7,10 @@ import hooytekken.skeleton.app.controller.HtekkenController;
 import hooytekken.skeleton.app.model.HTekkenModel;
 import hooytekken.skeleton.app.view.MenuScreen;
 
+/**
+ * ApplicationListener/Game class for the game
+ * connects the model, controller and the view
+ */
 public class Hoytekken extends Game {
     public static final int TILE_SIZE = 32;
     public static final int V_WIDTH = TILE_SIZE * 30;
