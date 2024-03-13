0d387b7bb1b3833bafe4274a2f955a1f3c22e87c
Mathias Ness <mathias1389@gmail.com>
2024-03-13 14:40:43 +0100

    fixture a and b

--- src/main/java/hooytekken/skeleton/app/model/components/CollisionDetector.java
+++ src/main/java/hooytekken/skeleton/app/model/components/CollisionDetector.java
@@ -3,32 +3,30 @@ package hooytekken.skeleton.app.model.components;
 import com.badlogic.gdx.physics.box2d.Contact;
 import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.Manifold;
 
 public class CollisionDetector implements ContactListener {
 
     @Override
     public void beginContact(Contact contact) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'beginContact'");
+        Fixture fixtureA = contact.getFixtureA();
+        Fixture fixtureB = contact.getFixtureB();
     }
 
     @Override
     public void endContact(Contact contact) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'endContact'");
+        //ignore end contact
     }
 
     @Override
     public void preSolve(Contact contact, Manifold oldManifold) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'preSolve'");
+        //ignore pre solve
     }
 
     @Override
     public void postSolve(Contact contact, ContactImpulse impulse) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'postSolve'");
+        //ignore post solve
     }
     
 }
