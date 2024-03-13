eff654094aac7ca81f925c02ba6dd94c4ae6de5d
Mathias Ness <mathias1389@gmail.com>
2024-03-13 14:58:34 +0100

    changed player shape from a circle to a box

--- src/main/java/hooytekken/skeleton/app/model/components/PlayerEntity/Player.java
+++ src/main/java/hooytekken/skeleton/app/model/components/PlayerEntity/Player.java
@@ -2,6 +2,8 @@ package hooytekken.skeleton.app.model.components.PlayerEntity;
 
 import hooytekken.skeleton.app.Hoytekken;
 
+import org.w3c.dom.css.Rect;
+
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
@@ -9,6 +11,7 @@ import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 
 /**
@@ -75,8 +78,17 @@ public class Player extends Sprite implements IPlayer {
         body.setUserData(this);
 
         FixtureDef fdef = new FixtureDef();
-        CircleShape shape = new CircleShape();
-        shape.setRadius(PLAYER_RADIUS);
+        //CircleShape shape = new CircleShape();
+        //shape.setRadius(PLAYER_RADIUS);
+        //change shape to a rectangle instead of a circle
+        
+        PolygonShape shape = new PolygonShape();
+
+        // Set the shape to be a box of the desired width and height
+        float width = 45 / Hoytekken.PPM; // replace with desired width
+        float height = 45 / Hoytekken.PPM; // replace with desired height
+        shape.setAsBox(width / 2, height / 2); // setAsBox takes half-width and half-height as arguments
+
 
         fdef.shape = shape;
         body.createFixture(fdef);
