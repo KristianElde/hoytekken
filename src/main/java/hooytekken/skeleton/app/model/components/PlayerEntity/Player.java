b5adb608895967e6e3e0d7a3cc7c37304e940e12
Mathias Ness <mathias1389@gmail.com>
2024-03-13 15:26:38 +0100

    set edgeshape feet to isSensor true, removed unused imports

--- src/main/java/hooytekken/skeleton/app/model/components/PlayerEntity/Player.java
+++ src/main/java/hooytekken/skeleton/app/model/components/PlayerEntity/Player.java
@@ -2,14 +2,11 @@ package hooytekken.skeleton.app.model.components.PlayerEntity;
 
 import hooytekken.skeleton.app.Hoytekken;
 
-import org.w3c.dom.css.Rect;
-
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.EdgeShape;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
@@ -88,6 +85,7 @@ public class Player extends Sprite implements IPlayer {
         EdgeShape feet = new EdgeShape();
         feet.set(new Vector2(-PLAYER_WIDTH / 2.1f, -PLAYER_HEIGHT / 2), new Vector2(PLAYER_WIDTH / 2.1f, -PLAYER_HEIGHT / 2));
         fdef.shape = feet;
+        fdef.isSensor = true;
         body.createFixture(fdef).setUserData(this.type + "feet");
     }
 
