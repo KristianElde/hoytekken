50ed31175942ce914d7d238dc4aec6b88868baf1
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-14 10:34:59 +0100

    added PlayerTest::sanityTest

--- src/test/java/hooytekken/skeleton/app/Model/ModelTest.java
+++ src/test/java/hooytekken/skeleton/app/Model/ModelTest.java
@@ -1,9 +1,12 @@
 package hooytekken.skeleton.app.Model;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
@@ -13,6 +16,7 @@ import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
 
 import hooytekken.skeleton.app.model.HTekkenModel;
+import hooytekken.skeleton.app.model.components.GameState;
 
 public class ModelTest {
     private HTekkenModel model;
@@ -29,5 +33,14 @@ public class ModelTest {
     @BeforeEach
     void setUpBeforeEach() {
         this.model = new HTekkenModel();
+    }
+
+    @Test
+    void sanityTest() {
+        assertNotNull(model.getGameState());
+        assertEquals(GameState.INSTRUCTIONS, model.getGameState());
+        assertNotNull(model.getGameWorld());
+        assertNotNull(model.getMap());
+        assertNotNull(model.getTiledMap());
     }
 }
