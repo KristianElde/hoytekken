25747e2c03cef755d3a86ff44b2bbf27944c5830
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-14 10:24:25 +0100

    added mock of GL20 in ModelTest

--- src/test/java/hooytekken/skeleton/app/Model/ModelTest.java
+++ src/test/java/hooytekken/skeleton/app/Model/ModelTest.java
@@ -1,11 +1,15 @@
 package hooytekken.skeleton.app.Model;
 
+import static org.mockito.Mockito.mock;
+
 import org.junit.jupiter.api.BeforeAll;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
 
 public class ModelTest {
     @BeforeAll
@@ -14,5 +18,6 @@ public class ModelTest {
         ApplicationListener listener = new ApplicationAdapter() {
         };
         new HeadlessApplication(listener, config);
+        Gdx.gl = mock(GL20.class);
     }
 }
