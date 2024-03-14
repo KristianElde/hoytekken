1082f4907defea3a6bcb3cd50d95563ca03c1a62
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-14 10:22:27 +0100

    added the headlessconfiguration for ModelTest

--- src/test/java/hooytekken/skeleton/app/Model/ModelTest.java
+++ src/test/java/hooytekken/skeleton/app/Model/ModelTest.java
@@ -1,5 +1,18 @@
 package hooytekken.skeleton.app.Model;
 
-public class ModelTest {
+import org.junit.jupiter.api.BeforeAll;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+
+public class ModelTest {
+    @BeforeAll
+    static void setUpBeforeAll() {
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        ApplicationListener listener = new ApplicationAdapter() {
+        };
+        new HeadlessApplication(listener, config);
+    }
 }
