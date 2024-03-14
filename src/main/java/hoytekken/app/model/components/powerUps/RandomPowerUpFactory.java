39acd05991edbf1422f7903c42434567690521e6
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-14 16:40:46 +0100

    made RandomPowerUpFactory implement PowerUpFactory

--- src/main/java/hoytekken/app/model/components/powerUps/RandomPowerUpFactory.java
+++ src/main/java/hoytekken/app/model/components/powerUps/RandomPowerUpFactory.java
@@ -1,5 +1,11 @@
 package hoytekken.app.model.components.powerUps;
 
-public class RandomPowerUpFactory {
+public class RandomPowerUpFactory implements PowerUpFactory {
+
+    @Override
+    public PowerUp getNext() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNext'");
+    }
 
 }
