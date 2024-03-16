b0d6b4c535697cf8d65e24baa70bb1f897cd009c
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-16 15:09:05 +0100

    implemented getNExt in RandomPUpfactory

--- src/main/java/hoytekken/app/model/components/powerUps/RandomPowerUpFactory.java
+++ src/main/java/hoytekken/app/model/components/powerUps/RandomPowerUpFactory.java
@@ -1,11 +1,16 @@
 package hoytekken.app.model.components.powerUps;
 
+import java.util.Random;
+
 public class RandomPowerUpFactory implements PowerUpFactory {
 
     @Override
     public PowerUp getNext() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getNext'");
+        String pUpString = "SDH";
+        Random rand = new Random();
+        Character c = pUpString.charAt(rand.nextInt(pUpString.length()));
+        return PowerUp.newPowerUp(c);
+
     }
 
 }
