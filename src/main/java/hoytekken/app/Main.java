a46ad32aac63dbe3a8f486005f26569641a67c91
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-03-18 11:58:29 +0100

    typo

--- src/main/java/hoytekken/app/Main.java
+++ src/main/java/hoytekken/app/Main.java
@@ -9,12 +9,12 @@ import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
  */
 public class Main {
     private static final int WINDOW_WIDTH = 1920;
-    private static final int WINDOW_HEIGTH = 1280;
+    private static final int WINDOW_HEIGHT = 1280;
 
     public static void main(String[] args) {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         cfg.setTitle("hoytekken");
-        cfg.setWindowedMode(WINDOW_WIDTH, WINDOW_HEIGTH);
+        cfg.setWindowedMode(WINDOW_WIDTH, WINDOW_HEIGHT);
 
         new Lwjgl3Application(new Hoytekken(), cfg);
     }
