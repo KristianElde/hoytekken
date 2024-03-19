1b6dc7385547dff2a3081b3a194c4412ca3549d5
Mathias Ness <mathias1389@gmail.com>
2024-03-19 16:29:49 +0100

    implemented resize to make sure instructions screen follows the scaling of the window

--- src/main/java/hoytekken/app/view/screens/InstructionsScreen.java
+++ src/main/java/hoytekken/app/view/screens/InstructionsScreen.java
@@ -2,6 +2,7 @@ package hoytekken.app.view.screens;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.ScreenAdapter;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -122,7 +123,7 @@ public class InstructionsScreen implements Screen {
 
     @Override
     public void resize(int width, int height) {
-        // ingore implementation
+        gamePort.update(width, height);
     }
 
     @Override
