c53b21da80c9189166eb8eed79b8aa497c6d33ff
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-19 09:21:37 +0100

    added file ExtraHealth

---
+++ src/main/java/hoytekken/app/model/components/powerup/ExtraHealth.java
@@ -0,0 +1,20 @@
+package hoytekken.app.model.components.powerup;
+
+import com.badlogic.gdx.graphics.Texture;
+
+import hoytekken.app.model.components.player.Player;
+
+public class ExtraHealth extends PowerUp {
+
+    protected ExtraHealth(PowerUpType pUpType, Texture pUpTexture) {
+        super(pUpType, pUpTexture);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void applyPowerUp(Player player) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'applyPowerUp'");
+    }
+
+}
