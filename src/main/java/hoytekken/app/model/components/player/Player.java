6b162bb6133d58675dc3ba2dd2a55040d758de00
jonas.bergaas <xub016@uib.no>
2024-03-20 16:24:08 +0100

    Remove unused imports

--- src/main/java/hoytekken/app/model/components/player/Player.java
+++ src/main/java/hoytekken/app/model/components/player/Player.java
@@ -1,7 +1,5 @@
 package hoytekken.app.model.components.player;
 
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
