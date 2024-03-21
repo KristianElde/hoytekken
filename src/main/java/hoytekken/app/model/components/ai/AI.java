3c5417ef224fad190f52b9c399d9809936163354
Sune.Lianes <sune.lianes@student.uib.no>
2024-03-21 15:20:50 +0100

    made AI extend player

--- src/main/java/hoytekken/app/model/components/ai/AI.java
+++ src/main/java/hoytekken/app/model/components/ai/AI.java
@@ -1,5 +1,15 @@
 package hoytekken.app.model.components.ai;
 
-public class AI {
-    
+import com.badlogic.gdx.physics.box2d.World;
+
+import hoytekken.app.model.components.player.Player;
+import hoytekken.app.model.components.player.PlayerType;
+
+public class AI extends Player {
+
+    public AI(World world, PlayerType type, int health) {
+        super(world, type, health);
+        // TODO Auto-generated constructor stub
+    }
+
 }
