c30f93f5d408ac1b37e2684f0ba350a0470dcca6
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-04-03 15:24:31 +0200

    created BaseScreenTest-class

---
+++ src/test/java/hoytekken/app/view/screens/BaseScreenTest.java
@@ -0,0 +1,15 @@
+package hoytekken.app.view.screens;
+
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+
+import hoytekken.app.Hoytekken;
+import hoytekken.app.view.ViewableModel;
+
+public class BaseScreenTest {
+    private static HeadlessApplication application;
+    private static Hoytekken gameMock;
+    private static ViewableModel modelMock;
+
+    private MenuScreen menuScreen;
+
+}
