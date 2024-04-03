c21b00bfd3b18771e82836416c1db6af4bb74324
Vetle Larsen <vetlelarzen@gmail.com>
2024-04-03 15:00:31 +0200

    Do block in method

--- src/main/java/hoytekken/app/model/components/powerup/PowerUpSpawn.java
+++ src/main/java/hoytekken/app/model/components/powerup/PowerUpSpawn.java
@@ -1,8 +1,11 @@
 package hoytekken.app.model.components.powerup;
 
+import org.lwjgl.system.MathUtil;
+
 import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer.Random;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Vector2;
 
 public class PowerUpSpawn {
@@ -19,5 +22,14 @@ public class PowerUpSpawn {
         int width = layer.getWidth();
         int height = layer.getHeight();
 
+        int x, y;
+        boolean isTileOccupied = true;
+        do {
+            x = MathUtils.random(width - 1);
+            y = MathUtils.random(height - 1);
+
+            //isTileOccupied = layer.getCell(x, y) != null;
+
+        }
     }
 }
