cf0318d732f16cf7e1bf4c654d55ffb751c4c7ec
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-04-03 15:27:04 +0200

    Added setUpBeforeAll and Each for BaseScreenTest

--- src/test/java/hoytekken/app/view/screens/BaseScreenTest.java
+++ src/test/java/hoytekken/app/view/screens/BaseScreenTest.java
@@ -1,6 +1,14 @@
 package hoytekken.app.view.screens;
 
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 import hoytekken.app.Hoytekken;
 import hoytekken.app.view.ViewableModel;
@@ -11,5 +19,22 @@ public class BaseScreenTest {
     private static ViewableModel modelMock;
 
     private MenuScreen menuScreen;
+
+    @BeforeAll
+    public static void setUpBeforeAll() {
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        application = new HeadlessApplication(new Hoytekken(), config);
+
+        // Mock dependencies
+        Gdx.gl = mock(GL20.class);
+        gameMock = mock(Hoytekken.class);
+        modelMock = mock(ViewableModel.class);
+        gameMock.batch = mock(SpriteBatch.class);
+    }
+
+    @BeforeEach
+    public void setUp() {
+        menuScreen = new MenuScreen(gameMock, modelMock);
+    }
 
 }
