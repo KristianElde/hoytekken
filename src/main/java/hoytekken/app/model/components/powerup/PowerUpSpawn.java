cbb78dd9b4c8ed6639fe82f859bfa408d0f7bbb4
Vetle Larsen <vetlelarzen@gmail.com>
2024-04-03 14:50:22 +0200

    Made a PowerUpSpawn class

---
+++ src/main/java/hoytekken/app/model/components/powerup/PowerUpSpawn.java
@@ -0,0 +1,22 @@
+package hoytekken.app.model.components.powerup;
+
+import com.badlogic.gdx.graphics.g3d.particles.influencers.ColorInfluencer.Random;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapTileLayer;
+import com.badlogic.gdx.math.Vector2;
+
+public class PowerUpSpawn {
+    // private TiledMap map;
+    // private RandomPowerUpFactory powerUpFactory;
+
+    // public PowerUpSpawn(TiledMap map) {
+    //     this.map = map;
+    //     this.powerUpFactory = new RandomPowerUpFactory();
+    // }
+
+    // public Vector2 getRandomPowerUpSpawn() {
+    //     TiledMapTileLayer layer = (TiledMapTileLayer) map.getLayers().get(0);
+    //     int width = layer.getWidth();
+    //     int height = layer.getHeight();
+    // }
+}
