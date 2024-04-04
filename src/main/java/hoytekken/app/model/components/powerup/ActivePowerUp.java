abd917061cde1aeb9aa47e1683c668fc56cd7cac
Vetle Larsen <vetlelarzen@gmail.com>
2024-04-04 15:10:29 +0200

    Almost copied the defineBody from Player

--- src/main/java/hoytekken/app/model/components/powerup/ActivePowerUp.java
+++ src/main/java/hoytekken/app/model/components/powerup/ActivePowerUp.java
@@ -5,7 +5,9 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 
 import com.badlogic.gdx.physics.box2d.Body;
-
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 import hoytekken.app.Hoytekken;
@@ -32,7 +34,24 @@ public class ActivePowerUp extends Sprite {
         setRegion(texture);
         defineBody();
         setBounds(0, 0, POWERUP_SIZE, POWERUP_SIZE);
-        
+        positionBody();
+        positionTexture();
+    }
+
+    private void defineBody() {
+        BodyDef bdef = new BodyDef();
+        bdef.type = BodyDef.BodyType.StaticBody;
+        body = world.createBody(bdef);
+
+        // Set the user data to this object
+        body.setUserData(this);
+
+        FixtureDef fdef = new FixtureDef();
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(POWERUP_SIZE / 2, POWERUP_SIZE / 2);
+        fdef.shape = shape;
+        fdef.isSensor = true;
+        body.createFixture(fdef).setUserData(this.type + "powerUp");
     }
 
     
