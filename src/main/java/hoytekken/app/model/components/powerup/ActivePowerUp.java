130f943ec206f16dfeef0d9dd4687c070055f3b6
Vetle Larsen <vetlelarzen@gmail.com>
2024-04-04 15:01:57 +0200

    Starting to implement the class

--- src/main/java/hoytekken/app/model/components/powerup/ActivePowerUp.java
+++ src/main/java/hoytekken/app/model/components/powerup/ActivePowerUp.java
@@ -1,5 +1,15 @@
 package hoytekken.app.model.components.powerup;
 
-public class ActivePowerUp {
+public class ActivePowerUp extends Sprite {
+
+    private static final float POWERUP_SIZE = 30 / Hoytekken.PPM;
+
+    private String type;
+    private PowerUpFactory factory;
+    private World world;
+    private Body body;
+    private Texture texture;
+
     
+
 }
