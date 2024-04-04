202ee66aba7493c2953657fe8b7a238f1fac1fb8
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-04-04 14:41:05 +0200

    Added SetUpBeforeAll method

--- src/test/java/hoytekken/app/model/components/ai/AIPlayerTest.java
+++ src/test/java/hoytekken/app/model/components/ai/AIPlayerTest.java
@@ -2,6 +2,12 @@ package hoytekken.app.model.components.ai;
 
 import java.util.Random;
 
+import org.junit.jupiter.api.BeforeAll;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class AIPlayerTest {
@@ -9,4 +15,11 @@ public class AIPlayerTest {
     private AIPlayer AIPlayer;
     private Random rand = new Random();
 
+    @BeforeAll
+    static void setUpBeforeAll() {
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        ApplicationListener listener = new ApplicationAdapter() {
+        };
+        new HeadlessApplication(listener, config);
+    }
 }
