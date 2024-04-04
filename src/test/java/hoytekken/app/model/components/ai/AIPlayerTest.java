85fcc825f2f225576d9bfc0d16238973e1175aa3
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-04-04 14:39:25 +0200

    Added field variables to AIPlayerTest

--- src/test/java/hoytekken/app/model/components/ai/AIPlayerTest.java
+++ src/test/java/hoytekken/app/model/components/ai/AIPlayerTest.java
@@ -1,5 +1,12 @@
 package hoytekken.app.model.components.ai;
 
+import java.util.Random;
+
+import com.badlogic.gdx.physics.box2d.World;
+
 public class AIPlayerTest {
+    private World world;
+    private AIPlayer AIPlayer;
+    private Random rand = new Random();
 
 }
