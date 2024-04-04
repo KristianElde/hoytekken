a97110b3ebd42b92ad464d492c89a50b354c6f64
Vetle Larsen <vetlelarzen@gmail.com>
2024-04-04 15:06:26 +0200

    Starting to fill in the constructor

--- src/main/java/hoytekken/app/model/components/powerup/ActivePowerUp.java
+++ src/main/java/hoytekken/app/model/components/powerup/ActivePowerUp.java
@@ -1,5 +1,15 @@
 package hoytekken.app.model.components.powerup;
 
+import com.badlogic.gdx.graphics.Texture;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
+import com.badlogic.gdx.physics.box2d.Body;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+import hoytekken.app.Hoytekken;
+
 public class ActivePowerUp extends Sprite {
 
     private static final float POWERUP_SIZE = 30 / Hoytekken.PPM;
@@ -9,6 +19,16 @@ public class ActivePowerUp extends Sprite {
     private World world;
     private Body body;
     private Texture texture;
+    private PowerUp powerUp;
+
+    public ActivePowerUp(PowerUpFactory factory, World world) {
+        this.factory = factory;
+        this.world = world;
+        this.powerUp = factory.getNext();
+        this.type = powerUp.getClass().getSimpleName();
+        this.texture = powerUp.getTexture();
+        
+    }
 
     
 
