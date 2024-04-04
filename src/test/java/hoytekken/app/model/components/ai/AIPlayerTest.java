6de0579acd360efbee27505084f7d35609db2c09
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-04-04 14:45:25 +0200

    Added SetUpBeforeEach method

--- src/test/java/hoytekken/app/model/components/ai/AIPlayerTest.java
+++ src/test/java/hoytekken/app/model/components/ai/AIPlayerTest.java
@@ -1,17 +1,28 @@
 package hoytekken.app.model.components.ai;
 
+import static org.mockito.Mockito.mock;
+
 import java.util.Random;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+
+import hoytekken.app.model.components.player.IPlayer;
+import hoytekken.app.model.components.player.Player;
+import hoytekken.app.model.components.player.PlayerType;
 
 public class AIPlayerTest {
     private World world;
+    private Player opposition;
     private AIPlayer AIPlayer;
     private Random rand = new Random();
 
@@ -21,5 +32,15 @@ public class AIPlayerTest {
         ApplicationListener listener = new ApplicationAdapter() {
         };
         new HeadlessApplication(listener, config);
+    }
+
+    @BeforeEach
+    void setUpBeforeEach() {
+        Gdx.gl = mock(GL20.class);
+
+        // create interface for world which gives better abstraction?
+        world = new World(new Vector2(0, 0), true); // no gravity right now
+        opposition = new Player(world, PlayerType.PLAYER_ONE, 99);
+        AIPlayer = new AIPlayer(world, PlayerType.PLAYER_TWO, 99, opposition);
     }
 }
