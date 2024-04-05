02bde8e85fdc2811097f6b3b88c8b511c485d451
Mathias Ness <mathias1389@gmail.com>
2024-04-05 13:10:06 +0200

    simple test for the gameworld, and test view has access to correct player attributes

--- src/test/java/hoytekken/app/model/ModelViewTests.java
+++ src/test/java/hoytekken/app/model/ModelViewTests.java
@@ -19,6 +19,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
 
 import hoytekken.app.model.components.GameState;
 import hoytekken.app.model.components.player.PlayerType;
@@ -26,6 +27,9 @@ import hoytekken.app.view.ViewableModel;
 
 
 public class ModelViewTests {
+    private static final Vector2 GRAVITY_VECTOR = new Vector2(0, -14);
+    private static final int INITIAL_BODY_COUNT = 2;
+
     private ViewableModel model;
 
     @BeforeAll
@@ -61,6 +65,14 @@ public class ModelViewTests {
     void testGetPlayer() {
         assertNotNull(model.getPlayer(PlayerType.PLAYER_ONE), "Player object should be initialized.");
         assertNotNull(model.getPlayer(PlayerType.PLAYER_TWO), "Player object should be initialized.");
+
+        //assert View has access to correct player attributes
+        assertDoesNotThrow(() -> model.getPlayer(PlayerType.PLAYER_ONE).getHealth());
+        assertDoesNotThrow(() -> model.getPlayer(PlayerType.PLAYER_TWO).getHealth());
+        assertDoesNotThrow(() -> model.getPlayer(PlayerType.PLAYER_ONE).getLives());
+        assertDoesNotThrow(() -> model.getPlayer(PlayerType.PLAYER_TWO).getLives());
+        assertDoesNotThrow(() -> model.getPlayer(PlayerType.PLAYER_ONE).isAlive());
+        assertDoesNotThrow(() -> model.getPlayer(PlayerType.PLAYER_TWO).isAlive());
     }
 
     @Test
@@ -76,7 +88,17 @@ public class ModelViewTests {
     @Test
     void testGetGameState() {
         assertNotNull(model.getGameState(), "Gamestate should be initialized.");
-        assertEquals(model.getGameState(), GameState.MAIN_MENU, "Gamestate should be the same.");
+        assertEquals(model.getGameState(), GameState.MAIN_MENU, "Intial game stata should me main menu.");
     }
-    
+
+    @Test
+    void testGetGameWorld() {
+        assertNotNull(model.getGameWorld(), "Game world should be initialized.");
+
+        Vector2 gravity = model.getGameWorld().getGravity();
+        int bodyCount = model.getGameWorld().getBodyCount();
+
+        assertEquals(gravity, GRAVITY_VECTOR, "Game world should have gravity vector set.");
+        assertEquals(bodyCount, INITIAL_BODY_COUNT, "Game world should have to player bodies");
+    }
 }
