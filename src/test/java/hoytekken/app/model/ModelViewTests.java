0a8fd7ae67eea708fedf19bc4b6726b4ad7164e5
Mathias Ness <mathias1389@gmail.com>
2024-04-05 12:43:08 +0200

    simple tests for update, player and map

--- src/test/java/hoytekken/app/model/ModelViewTests.java
+++ src/test/java/hoytekken/app/model/ModelViewTests.java
@@ -9,6 +9,7 @@ import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
@@ -17,9 +18,12 @@ import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
 
+import hoytekken.app.model.components.player.PlayerType;
+import hoytekken.app.view.ViewableModel;
+
 
 public class ModelViewTests {
-    private HTekkenModel model;
+    private ViewableModel model;
 
     @BeforeAll
     static void setUpBeforeAll() {
@@ -36,4 +40,27 @@ public class ModelViewTests {
 
     }
 
+    @Test
+    void sanityTest() {
+        assertEquals(1, 1, "Sanity check to verify that tests are working.");
+        assertNotNull(Gdx.gl, "Mock GL20 object should be initialized.");
+        assertNotNull(model, "Model object should be initialized.");
+    }
+
+    @Test
+    void testUpdateModel() {
+        model.updateModel(0.1f);
+        assertNotNull(model, "Model object should be initialized.");
+    }
+
+    @Test
+    void testGetPlayer() {
+        assertNotNull(model.getPlayer(PlayerType.PLAYER_ONE), "Player object should be initialized.");
+        assertNotNull(model.getPlayer(PlayerType.PLAYER_TWO), "Player object should be initialized.");
+    }
+
+    @Test
+    void testGetMap() {
+        assertNotNull(model.getMap(), "Map object should be initialized.");
+    }
 }
