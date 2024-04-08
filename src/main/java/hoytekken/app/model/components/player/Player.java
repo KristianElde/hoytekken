514154ec05160757b8e7308a742aaf77ccbab173
Lauritz Angeltveit <laang9684@uib.no>
2024-04-08 13:37:20 +0200

    Fjernet kommentering i konstruktør

--- src/main/java/hoytekken/app/model/components/player/Player.java
+++ src/main/java/hoytekken/app/model/components/player/Player.java
@@ -71,8 +71,6 @@ public class Player extends Sprite implements IPlayer {
      * @param health the health of the player
      */
     public Player(World world, PlayerType type, int health) {
-        //super(atlas.findRegion("Character_1_normalStand(60x27)"));
-        //atlas2.findRegion("Character_2_normalStand(60x27)");
         super(type == PlayerType.PLAYER_ONE 
         ? atlas.findRegion("Character_1_normalStand(60x27)") 
         : atlas2.findRegion("Character_2_normalStand(60x27)"));
