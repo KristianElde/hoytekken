270f5a3bbacc38c8d65ec3b743d5da199f6ac68b
Lauritz Angeltveit <laang9684@uib.no>
2024-04-08 13:16:35 +0200

    La til atlas2.findregion i konstruktør

--- src/main/java/hoytekken/app/model/components/player/Player.java
+++ src/main/java/hoytekken/app/model/components/player/Player.java
@@ -71,6 +71,7 @@ public class Player extends Sprite implements IPlayer {
      */
     public Player(World world, PlayerType type, int health) {
         super(atlas.findRegion("Character_1_normalStand(60x27)"));
+        atlas2.findRegion("Character_2_normalStand(60x27)");
         this.world = world;
         this.type = type;
         this.health = health;
