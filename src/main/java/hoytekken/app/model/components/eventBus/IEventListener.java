ea32bc4be165bc0a83b0f5643241227998a96714
Lauritz Angeltveit <laang9684@uib.no>
2024-04-10 16:21:02 +0200

    La til en metode handleEvent  IEventListener

--- src/main/java/hoytekken/app/model/components/eventBus/IEventListener.java
+++ src/main/java/hoytekken/app/model/components/eventBus/IEventListener.java
@@ -2,4 +2,9 @@ package hoytekken.app.model.components.eventBus;
 
 public interface IEventListener {
     
+    /**
+     * Handles an event
+     * @param event the event to handle
+     */
+    public void handleEvent (IEvent event);
 }
