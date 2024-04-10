e1e1bfb90934f61ce13e7f661c3902c999e666a6
Lauritz Angeltveit <laang9684@uib.no>
2024-04-10 12:01:01 +0200

    Bundet kickanimation til en ny animation

--- src/main/java/hoytekken/app/model/components/player/Player.java
+++ src/main/java/hoytekken/app/model/components/player/Player.java
@@ -100,6 +100,8 @@ public class Player extends Sprite implements IPlayer {
         // Kicking animation
         frames.add(new TextureRegion(getTexture(), 1512, 0, 666, 1080));
         frames.add(new TextureRegion(getTexture(), 360, 0, 666, 1080));
+        kickAnimation = new Animation<TextureRegion>(0.1f, frames);
+        //frames.clear();
 
         this.playerStand = new TextureRegion(getTexture(), 1026, 0, 486, 1080);
 
