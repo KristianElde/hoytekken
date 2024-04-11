4869ff86ee3d1fd75bd967ec862fca41a5d624fa
Mathias Ness <mathias1389@gmail.com>
2024-04-11 15:23:00 +0200

    method to draw maps, test with background image

--- src/main/java/hoytekken/app/view/screens/SelectionScreen.java
+++ src/main/java/hoytekken/app/view/screens/SelectionScreen.java
@@ -1,22 +1,59 @@
 package hoytekken.app.view.screens;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 
 import org.lwjgl.opengl.GL20;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 
 import hoytekken.app.Hoytekken;
 import hoytekken.app.view.ViewableModel;
 
 public class SelectionScreen extends BaseScreen {
-    private HashMap<String, String> gameMaps;
+    private LinkedList<Texture> mapTextures = new LinkedList<Texture>();
     
     public SelectionScreen(Hoytekken game, ViewableModel model) {
         super(game, model);
-        gameMaps = model.getGameMaps();
+        loadMapTextures();
         
     }
+
+    private void loadMapTextures() {
+        /*mapTextures.add(new Texture(Gdx.files.internal("map1.png")));
+        mapTextures.add(new Texture(Gdx.files.internal("map2.png")));
+        mapTextures.add(new Texture(Gdx.files.internal("map3.png")));
+        mapTextures.add(new Texture(Gdx.files.internal("map4.png")));*/
+        int count = 0;
+        while(count < 4) {
+            mapTextures.add(new Texture(Gdx.files.internal("background.png")));
+            count++;
+        }
+    }
+
+    private void drawMapSelections() {
+
+    // Calculate cell width and height based on the screen size and desired grid layout
+    float cellWidth = gamePort.getWorldWidth() / 2; // 2 columns
+    float cellHeight = gamePort.getWorldHeight() / 3; // 3 rows
+
+    // Starting positions for the bottom 2 rows
+    float startX = 0;
+    float startY = cellHeight;
+
+    int mapIndex = 0;
+
+    while(!mapTextures.isEmpty()) {
+        Texture mapTexture = mapTextures.poll();
+        float x = startX + (mapIndex % 2) * cellWidth;
+        float y = startY + (mapIndex / 2) * cellHeight;
+
+        game.batch.draw(mapTexture, x, y, cellWidth, cellHeight);
+
+        mapIndex++;
+    }
+}
 
     @Override
     public void render(float delta) {
@@ -28,7 +65,7 @@ public class SelectionScreen extends BaseScreen {
         game.batch.setProjectionMatrix(gameCam.combined);
 
         game.batch.begin();
-        //game.batch.draw(background, 0, 0, gamePort.getWorldWidth(), gamePort.getWorldHeight());
+        drawMapSelections();
         game.batch.end();
     }
 }
