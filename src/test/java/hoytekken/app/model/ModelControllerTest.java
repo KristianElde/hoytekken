ab709afacec434d516b0056ac02bce2057f4a4a7
Vetle Larsen <vetlelarzen@gmail.com>
2024-04-11 15:12:36 +0200

    BeforeEach

--- src/test/java/hoytekken/app/model/ModelControllerTest.java
+++ src/test/java/hoytekken/app/model/ModelControllerTest.java
@@ -3,6 +3,7 @@ package hoytekken.app.model;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
@@ -12,6 +13,7 @@ import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
 
 import hoytekken.app.controller.ControllableModel;
+import hoytekken.app.model.components.eventBus.EventBus;
 
 public class ModelControllerTest {
     private ControllableModel model;
@@ -22,6 +24,11 @@ public class ModelControllerTest {
         ApplicationListener listener = new ApplicationAdapter() {};
             new HeadlessApplication(listener, config);
             Gdx.gl = mock(GL20.class);
+    }
+
+    @BeforeEach
+    void setUpBeforeEach() {
+        model = new HTekkenModel(new EventBus());
     }
 
     
