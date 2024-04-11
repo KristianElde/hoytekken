3932521c9ebc5a2788aaaececedc6320cb11c8be
jonas.bergaas <xub016@uib.no>
2024-04-11 14:25:21 +0200

    Basic sound class structure

--- src/main/java/hoytekken/app/model/components/sound/Sound.java
+++ src/main/java/hoytekken/app/model/components/sound/Sound.java
@@ -1,5 +1,17 @@
-package hoytekken.app.model.components.sound;
-
 public class Sound {
+    private String soundName;
+    private String soundPath;
 
+    public Sound(String soundName, String soundPath) {
+        this.soundName = soundName;
+        this.soundPath = soundPath;
+    }
+
+    public String getSoundName() {
+        return soundName;
+    }
+
+    public String getSoundPath() {
+        return soundPath;
+    }
 }
