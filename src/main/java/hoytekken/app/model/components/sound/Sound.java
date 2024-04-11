c94399ec6fb18a2902168b291a8ba5f40beadb8e
jonas.bergaas <xub016@uib.no>
2024-04-11 14:27:57 +0200

    Implement method for play sound

--- src/main/java/hoytekken/app/model/components/sound/Sound.java
+++ src/main/java/hoytekken/app/model/components/sound/Sound.java
@@ -2,12 +2,17 @@ package hoytekken.app.model.components.sound;
 
 import com.badlogic.gdx.Gdx;
 
-public class Sound {
+public class Sound implements ISound {
     private com.badlogic.gdx.audio.Sound sound;
     private long id;
 
     public Sound(String path) {
         this.sound = Gdx.audio.newSound(Gdx.files.internal(path));
+    }
+
+    @Override
+    public void play() {
+        this.id = this.sound.play();
     }
 
 }
