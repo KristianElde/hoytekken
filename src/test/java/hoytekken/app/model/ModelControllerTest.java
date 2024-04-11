279c612aca0640dc536ea0a919a226dbe3862207
Vetle Larsen <vetlelarzen@gmail.com>
2024-04-11 15:09:23 +0200

    BeforeAll in tests

--- src/test/java/hoytekken/app/model/ModelControllerTest.java
+++ src/test/java/hoytekken/app/model/ModelControllerTest.java
@@ -1,5 +1,28 @@
 package hoytekken.app.model;
 
+import static org.mockito.Mockito.mock;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+import com.badlogic.gdx.graphics.GL20;
+
+import hoytekken.app.controller.ControllableModel;
+
 public class ModelControllerTest {
+    private ControllableModel model;
+
+    @BeforeAll
+    static void setUpBeforeAll() {
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        ApplicationListener listener = new ApplicationAdapter() {};
+            new HeadlessApplication(listener, config);
+            Gdx.gl = mock(GL20.class);
+    }
+
     
 }
