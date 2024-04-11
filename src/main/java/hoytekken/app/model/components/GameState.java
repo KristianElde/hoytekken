66e1d6dcafa746bbdf2b7af903fa021376d40d6f
Mathias Ness <mathias1389@gmail.com>
2024-04-11 15:40:56 +0200

    draws four images representing the maps. chenged the initial gamestate for testing purposes

--- src/main/java/hoytekken/app/model/components/GameState.java
+++ src/main/java/hoytekken/app/model/components/GameState.java
@@ -4,5 +4,5 @@ package hoytekken.app.model.components;
  * Enum for the game states
  */
 public enum GameState {
-    MAIN_MENU, ACTIVE_GAME, GAME_OVER, INSTRUCTIONS
+    MAIN_MENU, ACTIVE_GAME, GAME_OVER, INSTRUCTIONS, SELECTION
 }
