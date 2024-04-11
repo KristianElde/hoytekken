de650fecfe6a3c19010062c923499f6bb115c9a4
jonas.bergaas <xub016@uib.no>
2024-04-11 14:34:52 +0200

    Basic test setup

--- src/test/java/hoytekken/app/model/components/sound/SoundTest.java
+++ src/test/java/hoytekken/app/model/components/sound/SoundTest.java
@@ -1,5 +1,27 @@
 package hoytekken.app.model.components.sound;
 
-public class SoundTest {
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.headless.HeadlessApplication;
+import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
+
+public class SoundTest {
+    private ISound sound;
+
+    @BeforeAll
+    static void setUpBeforeAll() {
+        HeadlessApplicationConfiguration config = new HeadlessApplicationConfiguration();
+        ApplicationListener listener = new ApplicationAdapter() {
+        };
+
+        new HeadlessApplication(listener, config);
+    }
+
+    @BeforeEach
+    void setUpBeforeEach() {
+        sound = new Sound("path");
+    }
 }
