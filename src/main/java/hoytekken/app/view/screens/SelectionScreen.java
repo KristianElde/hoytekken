64ef0c25741f8ecef5231ea67897da3b3f931704
Mathias Ness <mathias1389@gmail.com>
2024-04-11 14:47:35 +0200

    new class Selection screen

---
+++ src/main/java/hoytekken/app/view/screens/SelectionScreen.java
@@ -0,0 +1,11 @@
+package hoytekken.app.view.screens;
+
+import hoytekken.app.Hoytekken;
+import hoytekken.app.view.ViewableModel;
+
+public class SelectionScreen extends BaseScreen {
+    
+    public SelectionScreen(Hoytekken game, ViewableModel model) {
+        super(game, model);
+    }
+}
