fe8c60cb5c47ab648a9cc0c893f426a25f8aea09
Lauritz Angeltveit <laang9684@uib.no>
2024-04-11 16:21:54 +0200

    La til en setup

--- src/test/java/hoytekken/app/HoytekkenTest.java
+++ src/test/java/hoytekken/app/HoytekkenTest.java
@@ -1,5 +1,16 @@
 package hoytekken.app;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mockito;
+
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
 public class HoytekkenTest {
     private Hoytekken game;
+
+    @BeforeEach
+    void setUp() {
+        game = new Hoytekken();
+        game.batch = Mockito.mock(SpriteBatch.class);
+    }
 }
