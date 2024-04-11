6d64b9fb760db476364610e2c8c763ac1d5a765e
Lauritz Angeltveit <laang9684@uib.no>
2024-04-11 16:22:28 +0200

    La til en sanity test

--- src/test/java/hoytekken/app/HoytekkenTest.java
+++ src/test/java/hoytekken/app/HoytekkenTest.java
@@ -1,6 +1,11 @@
 package hoytekken.app;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -12,5 +17,13 @@ public class HoytekkenTest {
     void setUp() {
         game = new Hoytekken();
         game.batch = Mockito.mock(SpriteBatch.class);
+    }
+
+    @Test
+    void sanityTest() {
+        assertTrue(true);
+        assertEquals(1, 1);
+        assertNotNull(game);
+        assertNotNull(game.batch);
     }
 }
