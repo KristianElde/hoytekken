0bd6b5fd105066b3ac73746cc469f4de46ddeebb
Mathias Ness <mathias1389@gmail.com>
2024-04-12 15:52:46 +0200

    moved methods to Abstract class

--- src/main/java/hoytekken/app/model/components/tools/AbstractCollision.java
+++ src/main/java/hoytekken/app/model/components/tools/AbstractCollision.java
@@ -1,11 +1,34 @@
 package hoytekken.app.model.components.tools;
 
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
 import com.badlogic.gdx.physics.box2d.ContactListener;
+import com.badlogic.gdx.physics.box2d.Manifold;
 
 public abstract class AbstractCollision implements ContactListener {
     protected HandleCollisions model;
 
     public AbstractCollision(HandleCollisions model) {
         this.model = model;
+    }
+
+    @Override
+    public void beginContact(Contact contact) {
+        // ignore begin contact
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+        // ignore end contact
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+        // ignore pre solve
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+        // ignore post solve
     }
 }
