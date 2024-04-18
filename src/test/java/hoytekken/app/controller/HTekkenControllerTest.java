9e643fc5ef8c21c75b5ffadb6ccd118d986a2d13
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-04-18 16:53:53 +0200

    added helper methods for moving players besides eachother

--- src/test/java/hoytekken/app/controller/HTekkenControllerTest.java
+++ src/test/java/hoytekken/app/controller/HTekkenControllerTest.java
@@ -16,7 +16,9 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.math.Vector2;
 
+import hoytekken.app.Hoytekken;
 import hoytekken.app.model.HTekkenModel;
 import hoytekken.app.model.components.GameState;
 import hoytekken.app.model.components.eventBus.EventBus;
@@ -45,6 +47,24 @@ public class HTekkenControllerTest {
         player1 = model.getPlayer(PlayerType.PLAYER_ONE);
         model.setNumberOfPlayers(false);
         player2 = model.getPlayer(PlayerType.PLAYER_TWO);
+    }
+
+    private boolean isWithinRange(IPlayer p1, IPlayer p2) {
+        float playerWidth = 45 / Hoytekken.PPM;
+
+        Vector2 p1Pos = new Vector2(p1.getBody().getPosition().x, p1.getBody().getPosition().y);
+        Vector2 p2Pos = new Vector2(p2.getBody().getPosition().x, p2.getBody().getPosition().y);
+
+        float distance = p1Pos.dst(p2Pos);
+        float range = playerWidth * 1.8f;
+        return distance <= range;
+    }
+
+    private void movePlayersBeside() {
+        while (!isWithinRange(player1, player2)) {
+            player1.move(100, 0);
+            model.getGameWorld().step(1 / 60f, 6, 2);
+        }
     }
 
     @Test
