ff33d42251ae62983aa1d0734891c2e35bdd47e3
Kristian.E.Johansen <kristian.e.johansen@student.uib.no>
2024-04-18 16:51:36 +0200

    added keyDownTest for ESC

--- src/test/java/hoytekken/app/controller/HTekkenControllerTest.java
+++ src/test/java/hoytekken/app/controller/HTekkenControllerTest.java
@@ -1,19 +1,23 @@
 package hoytekken.app.controller;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.mock;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.backends.headless.HeadlessApplication;
 import com.badlogic.gdx.backends.headless.HeadlessApplicationConfiguration;
 import com.badlogic.gdx.graphics.GL20;
 
 import hoytekken.app.model.HTekkenModel;
+import hoytekken.app.model.components.GameState;
 import hoytekken.app.model.components.eventBus.EventBus;
 import hoytekken.app.model.components.player.IPlayer;
 import hoytekken.app.model.components.player.PlayerType;
@@ -40,6 +44,18 @@ public class HTekkenControllerTest {
         player1 = model.getPlayer(PlayerType.PLAYER_ONE);
         model.setNumberOfPlayers(false);
         player2 = model.getPlayer(PlayerType.PLAYER_TWO);
+    }
+
+    @Test
+    void keyDownTestMainMenuI() {
+        model.setGameState(GameState.MAIN_MENU);
+        assertTrue(controller.keyDown(Input.Keys.I));
+    }
+
+    @Test
+    void keyDownTestMainMenuESCAPE() {
+        model.setGameState(GameState.MAIN_MENU);
+        assertTrue(controller.keyDown(Input.Keys.ESCAPE));
     }
 
 }
