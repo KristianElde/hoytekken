125261527d5ef1ae5ba3a891eef8d90eb6c4993a
Lauritz Angeltveit <laang9684@uib.no>
2024-04-25 15:50:26 +0200

    La in setup

--- src/test/java/hoytekken/app/model/components/eventBus/EventBusTest.java
+++ src/test/java/hoytekken/app/model/components/eventBus/EventBusTest.java
@@ -1,8 +1,21 @@
 package hoytekken.app.model.components.eventBus;
 
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+
 public class EventBusTest {
     private EventBus eventBus;
     private IEventListener mockListener;
     private IEvent mockEvent;
     
+    @BeforeEach
+    public void setUp() {
+        eventBus = new EventBus();
+        mockListener = mock(IEventListener.class); 
+        mockEvent = mock(IEvent.class);
+    }
+
+    
+
 }
