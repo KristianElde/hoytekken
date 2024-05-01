68526591acc4de15ce6e6e69c31affa9f6db25f6
Vetle Larsen <vetlelarzen@gmail.com>
2024-05-01 16:14:50 +0200

    Finished implementation

--- src/main/java/hoytekken/app/model/components/player/Player.java
+++ src/main/java/hoytekken/app/model/components/player/Player.java
@@ -367,9 +367,6 @@ public class Player extends Sprite implements IPlayer {
 
     @Override
     public void increaseHealth(int increaseAmount) {
-        // this.maxHealth += increaseAmount;
-        // this.health += increaseAmount;
-
         if (this.health + increaseAmount > this.maxHealth) {
             this.health = this.maxHealth;
         } else {
