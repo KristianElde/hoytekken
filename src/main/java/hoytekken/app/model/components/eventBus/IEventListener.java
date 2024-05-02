73e8c32ccb38705105ab2be32dc4da20cb828400
jonas.bergaas <xub016@uib.no>
2024-05-02 12:21:49 +0200

    Update handleEvent interface to throw illegal argument exception

--- src/main/java/hoytekken/app/model/components/eventBus/IEventListener.java
+++ src/main/java/hoytekken/app/model/components/eventBus/IEventListener.java
@@ -4,10 +4,11 @@ package hoytekken.app.model.components.eventBus;
  * Interface that represents an event listener
  */
 public interface IEventListener {
-    
+
     /**
      * Handles an event
+     * 
      * @param event the event to handle
      */
-    void handleEvent (IEvent event);
+    void handleEvent(IEvent event) throws IllegalArgumentException;
 }
